446e9c4628e333bdaa55f38595046355d09e41e8
Tasos Derisiotis <50984242+Eengineer1@users.noreply.github.com>
2025-04-01 19:39:46 +0300

    fix: Unique error codes registered

--- x/feeabs/keeper/ibc.go
+++ x/feeabs/keeper/ibc.go
@@ -210,12 +210,12 @@ func (k Keeper) GetChannelID(ctx sdk.Context) string {
 
 func (k Keeper) GetDecTWAPFromBytes(bz []byte) (sdk.Dec, error) {
 	if bz == nil {
-		return sdk.Dec{}, sdkerrors.New("GetDecTWAPFromBytes: err ", 1, "nil bytes")
+		return sdk.Dec{}, sdkerrors.New("GetDecTWAPFromBytes: err ", 101001, "nil bytes")
 	}
 	var ibcTokenTwap types.QueryArithmeticTwapToNowResponse
 	err := k.cdc.Unmarshal(bz, &ibcTokenTwap)
 	if err != nil || ibcTokenTwap.ArithmeticTwap.IsNil() {
-		return sdk.Dec{}, sdkerrors.New("arithmeticTwap data umarshal", 1, err.Error())
+		return sdk.Dec{}, sdkerrors.New("arithmeticTwap data umarshal", 101002, err.Error())
 	}
 	return ibcTokenTwap.ArithmeticTwap, nil
 }
