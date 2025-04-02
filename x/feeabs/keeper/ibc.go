20551574d86587da053e4d0cc2369996af0bcfb9
Tasos Derisiotis <50984242+Eengineer1@users.noreply.github.com>
2025-04-02 18:19:59 +0300

    fix: Switch to packet balue instead (corrected behaviour)

--- x/feeabs/keeper/ibc.go
+++ x/feeabs/keeper/ibc.go
@@ -149,9 +149,10 @@ func (k Keeper) OnAcknowledgementPacket(ctx sdk.Context, ack channeltypes.Acknow
 				continue
 			}
 			k.Logger(ctx).Info(fmt.Sprintf("ICQ response %+v", icqRes))
+			// Deprecated: icqRes.Key is not used anymore as of 2nd April 2025
 			// Not sure why, but the value is unmarshalled to icqRes.Key instead of icqRes.Value
 			// 10:36AM INF ICQ response {Code:0 Log: Info: Index:0 Key:[10 19 50 49 52 50 56 53 55 49 52 48 48 48 48 48 48 48 48 48 48] Value:[] ProofOps:<nil> Height:0 Codespace:}
-			twapRate, err := k.GetDecTWAPFromBytes(icqRes.Key)
+			twapRate, err := k.GetDecTWAPFromBytes(icqRes.Value)
 			if err != nil {
 				k.Logger(ctx).Error(fmt.Sprintf("Failed to get twap %s", err.Error()))
 				continue
